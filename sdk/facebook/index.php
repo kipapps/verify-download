2cd39be1b5c851ab52516e1fa98d193111e8c7cb
Kip <ki2thap@gmail.com>
2017-03-21 23:58:05 -0400

    make it better

---
+++ sdk/facebook/index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Verify Social
+Plugin URI: http://kipsoft.com
+Description: Make sure a user is liking subscribed or following you before viewing certain content.
+Author: KipSoft
+Version: 1.0
+Author URI: http://kipsoft.com
+*/
+
+?>
